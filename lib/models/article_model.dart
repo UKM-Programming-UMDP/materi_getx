93f81392700b0ed63ecf11b299037c58194b94c6
Luvianto <126235306+Luvianto@users.noreply.github.com>
2025-04-11 06:26:46 +0700

    Added Models

---
+++ lib/models/article_model.dart
@@ -0,0 +1,36 @@
+import 'package:materi_getx/models/source_model.dart';
+
+class ArticleModel {
+  final SourceModel source;
+  final String author;
+  final String title;
+  final String description;
+  final String url;
+  final String urlToImage;
+  final DateTime publishedAt;
+  final String content;
+
+  ArticleModel({
+    required this.source,
+    required this.author,
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.urlToImage,
+    required this.publishedAt,
+    required this.content,
+  });
+
+  factory ArticleModel.fromJson(Map<String, dynamic> json) {
+    return ArticleModel(
+      source: SourceModel.fromJson(json['source']),
+      author: json['author'],
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      urlToImage: json['urlToImage'],
+      publishedAt: DateTime.parse(json['publishedAt']),
+      content: json['content'],
+    );
+  }
+}

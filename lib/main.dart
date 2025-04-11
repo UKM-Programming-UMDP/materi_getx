1c557d82087535cd18e5e0d182f1b50511c5d25d
Luvianto <126235306+Luvianto@users.noreply.github.com>
2025-04-11 07:08:28 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Home(),
     );
   }
